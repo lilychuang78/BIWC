051b65eb7c220ed5f91df934e5a1d417e72290cb
unknown <lilychuang78@gmail.com>
2026-08-24 21:30:08 +0200

    add dynamic view of tablet and smartphone

--- js/components.js
+++ js/components.js
@@ -1,3 +1,6 @@
+const isRootPage = window.location.pathname.replace(/\/+$/, '').split('/').filter(Boolean).length <= 1;
+const componentBase = isRootPage ? 'components/' : '../components/';
+
 async function loadComponent(id, path) {
   const response = await fetch(path);
 
@@ -5,10 +8,18 @@ async function loadComponent(id, path) {
     throw new Error(`Could not load ${path}: ${response.status}`);
   }
 
-  document.getElementById(id).innerHTML = await response.text();
+  const html = await response.text();
+  document.getElementById(id).innerHTML = html;
+
+  if (id === 'site-header') {
+    const homeLink = document.querySelector('[data-home-link]');
+    if (homeLink) {
+      homeLink.setAttribute('href', isRootPage ? './' : '../');
+    }
+  }
 }
 
 Promise.all([
-  loadComponent("site-header", "/BIWC/components/header.html"),
-  loadComponent("site-footer", "/BIWC/components/footer.html")
+  loadComponent('site-header', `${componentBase}header.html`),
+  loadComponent('site-footer', `${componentBase}footer.html`)
 ]).catch(console.error);
